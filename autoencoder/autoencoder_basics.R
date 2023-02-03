8efad30ae1b40d2f74a27bffed6db72aa29e87eb
Davin Dillon <30303781+DavinDillon@users.noreply.github.com>
2023-02-03 15:35:13 -0600

    wrap autoencoder in function and create gifs with different options
    - optimizers sgd and adam
    - loss function mse, hinge, binary_crossentropy
    - output activation linear, sigmoid

--- autoencoder/autoencoder_basics.R
+++ autoencoder/autoencoder_basics.R
@@ -3,18 +3,20 @@ library(ggplot2)
 library(gganimate)
 library(gifski)
 
-setwd("~/Desktop/Baylor/Interns/keras_demo/autoencoder")
+
 
 ## Scale Data
 df <- read.csv("../data.csv")
 x_train <- df[, -c(1, 2, 33)]
 labels <- df$diagnosis
 ifelse(labels == "M", "Malignant", "Benign")
-x_scaled <- scale(X_train)
+x_scaled <- scale(x_train)
 
-input_size <- dim(X_scaled)[2]
+input_size <- dim(x_scaled)[2]
 latent_size <- 2
 
+
+aen_fc <- function(opt = "adam", lss = "mse", out_act = "linear"){
 enc_input = layer_input(shape = input_size)
 enc_output = enc_input %>% 
   layer_dense(units = 10, activation = "relu") %>%
@@ -25,7 +27,7 @@ encoder = keras_model(enc_input, enc_output)
 dec_input = layer_input(shape = latent_size)
 dec_output = dec_input %>% 
   layer_dense(units = 10, activation = "relu") %>% 
-  layer_dense(units = input_size, activation = "linear") 
+  layer_dense(units = input_size, activation = out_act) 
 
 decoder = keras_model(dec_input, dec_output)
 
@@ -36,7 +38,7 @@ aen_output = enc_input %>%
 aen = keras_model(enc_input, aen_output)
 summary(aen)
 
-aen %>% compile(optimizer = "adam", loss = "mse")
+aen %>% compile(optimizer = opt, loss = lss)
 
 # aen %>% fit(x_scaled, x_scaled, epochs = 100, batch_size = 32, shuffle = TRUE,
 #             validation_split = 0.5)
@@ -45,16 +47,17 @@ embed <- list()
 global_loss <- list()
 for (i in 1:100){
   global_loss[[i]] <- aen %>% fit(x_scaled, x_scaled, epochs = 1, batch_size=16,
-              validation_split = 0.2)
+                                  validation_split = 0.2)
   
-  embed[[i]] <- data.frame(predict(encoder, X_scaled), labels, i)
+  embed[[i]] <- data.frame(predict(encoder, x_scaled), labels, i)
 }
 
 x_decoded <- predict(aen, x_scaled)
 
+
 par(mfrow = c(1, 2))
-boxplot(X_scaled[, 1:5])
-boxplot(X_decoded[, 1:5])
+boxplot(x_scaled[, 1:5])
+boxplot(x_decoded[, 1:5])
 
 plot_df <- do.call(rbind, embed)
 colnames(plot_df)[4] <- "epoch"
@@ -67,10 +70,18 @@ p <- ggplot(plot_df, aes(X1, X2, color = labels)) +
   exit_shrink() +
   ease_aes('sine-in-out')
 animate(p, renderer = gifski_renderer())
-anim_save("file.gif", p)
+anim_save(paste(opt,"_", lss,"_", out_act,".gif"), p)
+}
+
+
+aen_fc("adam", "mse", "gelu")
+
+#anim_save("adam_mse_gelu_file.gif", p)
+
 
 
 ### Task for D:: Exploration
 # Explore various Keras Loss Functions, Optimizers and activation functions and
 # Save the embedding gifs to explore how it affects the latent space
+
 
